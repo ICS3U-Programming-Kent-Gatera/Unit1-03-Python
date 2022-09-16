7d5bbc45a960fc513710ce4425b64cdaedaa6860
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-09-16 00:05:47 +0000

    Added Hello World with style

---
+++ hello_world_style.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Created By: Kent Gatera
+# Date: 09.15.22
+# Hello world, with style.
+
+
+def main():
+    print("Hello World, with proper style!")
+    print("I am testing the function here!")
+
+if __name__ == "__main__":
+    main()
